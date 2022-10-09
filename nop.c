5c30327d2cd15b1f7afc1f4e1bc7d2acb40606a3
theeduke <oscarian528@gmail.com>
2022-10-09 12:41:43 -0700

    nop.c

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop -  does nothing
+ * @stack: doesnt matter
+ * @line_nums: for nothing
+ *
+ * Return: void
+ */
+void nop(stack_t **stack, unsigned int line_nums)
+{
+	(void) stack;
+	(void) line_nums;
+}
