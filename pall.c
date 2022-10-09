6ae510b5ea88a1bac2e205752b0fda53ff98d9ae
theeduke <oscarian528@gmail.com>
2022-10-09 12:44:43 -0700

    pall.c

--- pall.c
+++ pall.c
@@ -3,7 +3,7 @@
 /**
  * pall - prints the stack
  * @stack: stack given by main in start.c
- * @line_nums: amount of lines
+ * @line_nums: amount of lines.
  * Return: void
  */
 void pall(stack_t **stack, unsigned int line_nums __attribute__((unused)))
