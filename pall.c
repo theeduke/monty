36b13cf36beb4e5831407a1250750ed47a17eeae
theeduke <oscarian528@gmail.com>
2022-10-09 12:43:35 -0700

    #include monty.h

---
+++ pall.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * pall - prints the stack
+ * @stack: stack given by main in start.c
+ * @line_nums: amount of lines
+ * Return: void
+ */
+void pall(stack_t **stack, unsigned int line_nums __attribute__((unused)))
+{
+	print_stack(*stack);
+}
